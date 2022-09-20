7fce9ff684e0e6501c07726749b8f55654404694
Alberto <aquintero88@gmail.com>
2022-09-20 16:02:17 +0200

    individual issues working

--- routes/collections.js
+++ routes/collections.js
@@ -26,7 +26,7 @@ router.get('/', isAuthenticated, async (req, res, next) => {
     }
   });
 
-// @desc    Create a collection
+// @desc    Create an event collection
 // @route   POST /
 // @access  Private
   router.post('/:id', isAuthenticated, async (req, res, next) => { 
@@ -44,6 +44,44 @@ router.get('/', isAuthenticated, async (req, res, next) => {
             }
         } else {
             const collection = await Collection.create({userId, events:id});
+            res.status(201).json({ data: collection })     
+        }                 
+      } catch (error) {
+        next(error);
+      }
+    });
+
+// @desc    Show  issue collection
+// @route   GET /
+// @access  Private
+router.get('/issue', isAuthenticated, async (req, res, next) => {   
+  const userId = req.payload._id 
+    try {
+        const userCollection = await Collection.findOne({ userId:userId }).populate("issues");
+        res.status(201).json({ data: userCollection })             
+    } catch (error) {
+      next(error);
+    }
+  });
+
+// @desc    Create Issues collection
+// @route   POST /
+// @access  Private
+  router.post('/issue/:id', isAuthenticated, async (req, res, next) => { 
+    const {id} = req.params  
+    const userId = req.payload._id  
+      try {          
+        const collection = await Collection.findOne({ userId: userId });
+        if(collection){
+            if(!collection.issues.includes(id)){
+                collection.issues.push(id);
+                collection.save();
+                res.status(201).json({ data: collection })   
+            } else {
+                res.status(201).json({ data: collection, message: "Issue is already on user's collection" })   
+            }
+        } else {
+            const collection = await Collection.create({userId, issues:id});
             res.status(201).json({ data: collection })     
         }                 
       } catch (error) {
--- seed/index.js
+++ seed/index.js
@@ -1,27 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Issue = require ('../models/Issue');
+const Event = require ('../models/Event');
 // Import the model
 
 // Place the array you want to seed
-const issues = [
-  {
-    //Batman: A Lonely Place of Dying 
-    "universe":"DC",
-    "name": "Batman #440",
-    "order":"1",           
-    "years": "1989",            
-    "image": "https://res.cloudinary.com/dtxouyldc/image/upload/v1662544300/House%20of%20Heroes/DC/Events/Batman:%20A%20Lonely%20Place%20of%20Dying/batman_440_knbj0t.jpg",
-    "complementaryImages": "",
-    "description": "'A Lonely Place of Dying' part 1! Batman pushes the limits following Jason Todd's death. Meanwhile, a mysterious individual stalks Batman and Nightwing. Continued in The New Titans (1988-1996) #60. Batman continues to act in an uncontrolled and undisciplined manner, unaware that Two-Face is trying to have him killed, or that someone has been tailing him and taking photos. This same someone attempts to find Dick Grayson, and hearing that Haly's Circus is closing, assumes that this will be Dick's destination.",             
-  }
+const events = [
+
 ]
 
 
 mongoose.connect(process.env.MONGO_URL)
   .then(x => console.log(`Connected to ${x.connection.name}`))
   .then(() => {
-    return Issue.create(issues)
+    return Event.create(events)
   })
   .then(() => {
     console.log('Seed done 🌱');
