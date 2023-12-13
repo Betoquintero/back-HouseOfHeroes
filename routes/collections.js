903e26d9c0ca7ef327c8d9957c57200647e0ed68
Alberto Quintero <aquintero88@gmail.com>
2023-12-13 12:40:10 +0100

    refactor: eliminate feature of adding a single issue to collections

--- routes/collections.js
+++ routes/collections.js
@@ -68,59 +68,5 @@ router.get('/delete-event/:id', isAuthenticated, async (req, res, next) => {
   }
 });    
 
-// @desc    Show  issue collection
-// @route   GET /
-// @access  Private
-router.get('/issue', isAuthenticated, async (req, res, next) => {   
-  const userId = req.payload._id 
-    try {
-        const userCollection = await Collection.findOne({ userId:userId }).populate("issues");
-        res.status(201).json({ data: userCollection })             
-    } catch (error) {
-      next(error);
-    }
-  });
-
-// @desc    Create Issues collection
-// @route   POST /
-// @access  Private
-  router.post('/issue/:id', isAuthenticated, async (req, res, next) => { 
-    const {id} = req.params  
-    const userId = req.payload._id  
-      try {          
-        const collection = await Collection.findOne({ userId: userId });
-        if(collection){
-            if(!collection.issues.includes(id)){
-                collection.issues.push(id);
-                collection.save();
-                res.status(201).json({ data: collection })   
-            } else {
-                res.status(201).json({ data: collection, message: "Issue is already on user's collection" })   
-            }
-        } else {
-            const collection = await Collection.create({userId, issues:id});
-            res.status(201).json({ data: collection })     
-        }                 
-      } catch (error) {
-        next(error);
-      }
-    });
-
-// @desc    Delete an issue collection
-// @route   GET /delete-issue/:id
-// @access  Public
-router.get('/delete-issue/:id', isAuthenticated, async (req, res, next) => {  
-  const { id } = req.params;
-  const userId = req.payload._id
-  try {
-    const userCollection = await Collection.findOne({userId: userId});
-    userCollection.issues.pull(id);
-    userCollection.save();    
-    res.status(202).json({ data: userCollection });
-  } catch (error) {
-    next(error);
-  }
-});
-
   module.exports = router;
 
